9f9b1b3bba96f7aaa713d3fdeedfa9cf6d00eee7
G3CG3 <george@ggeorgiadis.gr>
2025-05-17 14:30:19 +0300

    Add AnimalSimulator main class with demo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import animalsimulator.Animal;
+import animalsimulator.Bird;
+import animalsimulator.Cat;
+import animalsimulator.Dog;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Animal dog = new Dog("Rex");
+        Animal cat = new Cat("Whiskers");
+        Animal bird = new Bird("Tweety");
+
+        Animal[] animals = { dog, cat, bird };
+
+        for (Animal animal : animals) {
+            System.out.println("---");
+            System.out.println("Animal: " + animal.getName() + " is a " + animal.getClass().getSimpleName());
+            animal.makeSound();
+            animal.move();
+            animal.eat();
+        }
     }
 }
